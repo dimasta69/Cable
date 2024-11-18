44a1b1bee4f4ee0b323aaaae0c4004a2ed29cf47
Долгополов Дмитрий <dmitry.dolgopolov@saltpepper.ru>
2024-11-18 17:29:35 +0300

    sdgfsdf

--- cabel/settings/database.py
+++ cabel/settings/database.py
@@ -6,11 +6,11 @@ load_dotenv()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "postgres",
-        'USER': "postgres",
-        'PASSWORD': 123,
-        'HOST': "postgres",
-        'PORT': 5432,
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
