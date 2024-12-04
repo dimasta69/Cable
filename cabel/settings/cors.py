9ae618249983a87828f94c52f6e4f4be05a3333c
Долгополов Дмитрий <dmitry.dolgopolov@saltpepper.ru>
2024-12-04 17:27:41 +0300

    dasdsa

--- cabel/settings/cors.py
+++ cabel/settings/cors.py
@@ -3,7 +3,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:8000",
     "http://127.0.0.1:8000"
 ]
-CSRF_TRUSTED_ORIGINS = ["http://212.67.12.63","http://localhost:8000","http://127.0.0.1:8000"]
+CSRF_TRUSTED_ORIGINS = ["http://212.67.12.63","http://localhost:8000","http://127.0.0.1:8000", "http://front:3000"]
 
 CORS_ALLOW_HEADERS = [
     "content-type",
