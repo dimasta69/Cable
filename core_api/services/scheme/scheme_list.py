08b54ce53a67c5f8a2e0b900e3ac80281ee7e698
Долгополов Дмитрий <dmitry.dolgopolov@saltpepper.ru>
2024-12-18 17:54:40 +0300

    fsdafsa

--- core_api/services/scheme/scheme_list.py
+++ core_api/services/scheme/scheme_list.py
@@ -1,5 +1,6 @@
 from service_objects.fields import ModelField
 from rest_framework import status
+from django import forms
 
 from models_app.models import User
 from models_app.models import Scheme
@@ -9,12 +10,19 @@ from utils.services import ServiceWithResult
 
 class SchemeListService(ServiceWithResult):
     current_user = ModelField(User)
+    search_filter = forms.CharField(required=False)
 
     def process(self):
         if self.is_valid():
-            self.result = self.scheme_list
+            self.result = self._filter_list
             self.response_status = status.HTTP_200_OK
         return self
+
+    def _filter_list(self):
+        scheme_list = self.scheme_list
+        if self.cleaned_data.get('search_filter'):
+            scheme_list = scheme_list.filter(scheme__title__icontains=self.cleaned_data["search_filter"])
+        return scheme_list
 
     @property
     def scheme_list(self):
