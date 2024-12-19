fe3b42a1b90e809ca165e8b163e20ca6639c2935
Долгополов Дмитрий <dmitry.dolgopolov@saltpepper.ru>
2024-12-19 16:01:30 +0300

    dsafsadgf

--- core_api/services/building/create.py
+++ core_api/services/building/create.py
@@ -61,8 +61,8 @@ class CreateBuildingService(ServiceWithResult):
 
     def number_presence(self):
         for building in self.building_list:
-            if building.number.lower() == self.cleaned_data['number'].lower():
-                self.add_error('number', ValidationError(f'Field with number={self.cleaned_data["number"]}'
+            if building.number.lower() == self.cleaned_data['name'].lower():
+                self.add_error('number', ValidationError(f'Field with number={self.cleaned_data["name"]}'
                                                          ' already exists'))
                 self.response_status = status.HTTP_422_UNPROCESSABLE_ENTITY
 
