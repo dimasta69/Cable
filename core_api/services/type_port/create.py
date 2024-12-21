66d0c0a0f1515fc015367046ac9a787ed4666ac0
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2024-12-21 19:11:22 +0300

    fsafg

---
+++ core_api/services/type_port/create.py
@@ -0,0 +1,30 @@
+from rest_framework.exceptions import PermissionDenied
+
+from utils.fields import ModelField
+from utils.services import ServiceWithResult
+from django.core.exceptions import ValidationError
+from rest_framework import status
+
+from django import forms
+from models_app.models import TypePort, User
+
+
+class CreateTypePort(ServiceWithResult):
+    name = forms.CharField(required=True)
+    current_user = ModelField(User)
+
+    custom_validations = ['unique_type',]
+
+    def process(self):
+        self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._create_type_port()
+        return self
+
+    def _create_type_port(self):
+        return TypePort.objects.create(name=self.cleaned_data['name'])
+
+    def unique_type(self):
+        if self.cleaned_data['name'] and TypePort.objects.filter(name=self.cleaned_data['name']):
+            self.add_error("name", ValidationError(f"Name = {self.cleaned_data['name']} is not unique"))
+            self.response_status = status.HTTP_400_BAD_REQUEST
--- core_api/views/type_port/type_port_list.py
+++ core_api/views/type_port/type_port_list.py
@@ -6,6 +6,7 @@ from drf_yasg.utils import swagger_auto_schema
 from utils.services import ServiceOutcome
 from core_api.serializers.type_port.type_port_list import TypePortListSerializer
 from core_api.services.type_port.type_port_list import TypePortListService
+from core_api.services.type_port.create import CreateTypePort
 
 
 class TypePortListView(APIView):
@@ -14,3 +15,9 @@ class TypePortListView(APIView):
     def get(self, request):
         outcome = ServiceOutcome(TypePortListService, request)
         return Response(TypePortListSerializer(outcome.result, many=True).data, status=outcome.response_status)
+
+    def post(self, request):
+        outcome = ServiceOutcome(CreateTypePort, request.data)
+        if bool(outcome.errors):
+            return Response(outcome.errors, status=outcome.response_status)
+        return Response(TypePortListSerializer(outcome.result).data, status=outcome.response_status)
