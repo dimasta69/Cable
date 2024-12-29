c5b512eedbd611809d76a275275c6d07e4bcc5b2
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2024-12-30 00:08:00 +0300

    dasd

--- core_api/views/equipment/equipment.py
+++ core_api/views/equipment/equipment.py
@@ -43,7 +43,7 @@ class EquipmentView(APIView):
 class ReleaseEquipmentView(APIView):
     permission_classes = [IsAuthenticated]
 
-    def put(self, request, **kwargs):
+    def path(self, request, **kwargs):
         outcome = ServiceOutcome(ReleaseEquipmentService, kwargs)
         if bool(outcome.errors):
             return Response(outcome.errors, status=outcome.response_status)
