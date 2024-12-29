67a255fca2486ad16855ff711e245bd400c4f4ca
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2024-12-29 07:27:11 +0300

    Delete line

--- core_api/services/port/connection.py
+++ core_api/services/port/connection.py
@@ -12,42 +12,43 @@ from utils.services import ServiceWithResult
 from models_app.models import Port, Line
 
 
-def port_json(port: Port) -> json:
+def port_json(port: Port, side: Literal["front_side", "back_side"]) -> json:
     return {
-            "port_id": str(port.pk),
-            "uid": str(port.uid),
-            "equipment_id": str(port.equipment.id),
-            "equipment_manufacturer": str(port.equipment.template.manufacturer),
-            "equipment_type": str(port.equipment.template.type),
-            "equipment_model": str(port.equipment.template.model),
+        "port_id": str(port.pk),
+        "side": side,
+        "uid": str(port.uid),
+        "equipment_id": str(port.equipment.id),
+        "equipment_manufacturer": str(port.equipment.template.manufacturer),
+        "equipment_type": str(port.equipment.template.type),
+        "equipment_model": str(port.equipment.template.model),
 
-            "server_rack_id": str(port.equipment.units.first().server_rack.id)
-            if not port.equipment.room else None,
+        "server_rack_id": str(port.equipment.units.first().server_rack.id)
+        if not port.equipment.room else None,
 
-            "unit_uuid": str(port.equipment.units.first().uid)
-            if not port.equipment.room else None,
+        "unit_uuid": str(port.equipment.units.first().uid)
+        if not port.equipment.room else None,
 
-            "unit_side": str(port.equipment.units.first().side)
-            if not port.equipment.room else None,
+        "unit_side": str(port.equipment.units.first().side)
+        if not port.equipment.room else None,
 
-            "room_id": str(port.equipment.units.first().server_rack.room.id)
-            if not port.equipment.room else str(port.equipment.room.id),
+        "room_id": str(port.equipment.units.first().server_rack.room.id)
+        if not port.equipment.room else str(port.equipment.room.id),
 
-            "room_floor": str(port.equipment.units.first().server_rack.room.floor)
-            if not port.equipment.room else str(port.equipment.room.floor),
+        "room_floor": str(port.equipment.units.first().server_rack.room.floor)
+        if not port.equipment.room else str(port.equipment.room.floor),
 
-            "room_number": str(port.equipment.units.first().server_rack.room.number)
-            if not port.equipment.room else str(port.equipment.room.number),
+        "room_number": str(port.equipment.units.first().server_rack.room.number)
+        if not port.equipment.room else str(port.equipment.room.number),
 
-            "is_server_room": str(port.equipment.units.first().server_rack.room.is_server_room)
-            if not port.equipment.room else str(port.equipment.room.is_server_room),
+        "is_server_room": str(port.equipment.units.first().server_rack.room.is_server_room)
+        if not port.equipment.room else str(port.equipment.room.is_server_room),
 
-            "building_id": str(port.equipment.units.first().server_rack.room.building.id)
-            if not port.equipment.room else str(port.equipment.room.bulding.id),
+        "building_id": str(port.equipment.units.first().server_rack.room.building.id)
+        if not port.equipment.room else str(port.equipment.room.bulding.id),
 
-            "building_name": str(port.equipment.units.first().server_rack.room.building.name)
-            if not port.equipment.room else str(port.equipment.room.bulding.name),
-        }
+        "building_name": str(port.equipment.units.first().server_rack.room.building.name)
+        if not port.equipment.room else str(port.equipment.room.bulding.name),
+    }
 
 
 class ConnectionPortService(ServiceWithResult):
@@ -59,54 +60,54 @@ class ConnectionPortService(ServiceWithResult):
     def process(self):
         self.run_custom_validations()
         if self.is_valid():
-            side = self._front_or_back_side()
-            port_1, port_2 = self._lines_is_null(side)
-            self._connection(port_1, port_2)
+            with transaction.atomic():
+                side = self._front_or_back_side()
+                port_1, port_2, side = self._lines_is_null(side)
+                line = self._create_line(port_1, port_2, side)
+                self._delete_past_line(port_1, port_2)
+                self._connection_port(line)
         return self
 
-    def _front_or_back_side(self) -> Literal['front_port_list', 'back_port_list']:
+    def _front_or_back_side(self) -> tuple[str, str]:
         if self.cleaned_data.get('front_port_list'):
-            return "front_port_list"
+            return "front_side", "front_port_list"
         elif self.cleaned_data.get('back_port_list'):
-            return "back_port_list"
+            return "back_side", "back_port_list"
 
-    def _lines_is_null(self, side: Literal['front_port_list', 'back_port_list']) -> Tuple[Port, Port]:
-        port_1 = self._ports.get(id=self.cleaned_data[side][0])
-        port_2 = self._ports.get(id=self.cleaned_data[side][1])
-        setattr(port_1, side, port_2)
-        setattr(port_2, side, port_1)
-        return port_1, port_2
+    def _lines_is_null(self, side: tuple[str, str]) -> Tuple[Port, Port, str]:
+        port_1 = self._ports.get(id=self.cleaned_data[side[1]][0])
+        port_2 = self._ports.get(id=self.cleaned_data[side[1]][1])
+        setattr(port_1, side[0], port_2)
+        setattr(port_2, side[0], port_1)
+        return port_1, port_2, side[0]
 
-    def _connection(self, port_1: Port, port_2: Port) -> None:
-        with transaction.atomic():
-            line_1 = None
-            line_2 = None
+    def _create_line(self, port_1: Port, port_2: Port, side: Literal["front_side", "back_side"]) -> Line:
+        if port_1.line:
+            line_1 = port_1.line.connection if port_1.line.connection[-1] == port_1.pk \
+                else list(reversed(port_1.line.connection))
+        else:
+            line_1 = [port_json(port_1, side)]
 
-            if port_1.line is None and port_2.line is None:
-                line_list = [port_json(port_1), port_json(port_2)]
-                line = Line.objects.create(connection=line_list)
-                port_1.line = line
-                port_2.line = line
-            else:
-                if port_1.line:
-                    line_1 = port_1.line.connection if port_1.line.connection[-1] == port_1.pk \
-                        else list(reversed(port_1.line.connection))
+        if port_2.line:
+            line_2 = port_2.line.connection if port_2.line.connection[0] == port_2.pk \
+                else list(reversed(port_2.line.connection))
+        else:
+            line_2 = [port_json(port_2, side)]
 
-                if port_2.line:
-                    line_2 = port_2.line.connection if port_2.line.connection[0] == port_2.pk \
-                        else list(reversed(port_2.line.connection))
+        return Line.objects.create(connection=line_1.extend(line_2))
 
-                line_1 = line_1 if line_1 else [port_json(port_1)]
-                line_2 = line_2 if line_2 else [port_json(port_2)]
+    def _delete_past_line(self, port_1: Port, port_2: Port) -> None:
+        if port_1.line:
+            port_1.line.delete()
+        if port_2.line:
+            port_2.line.delete()
 
-                line_1.extend(line_2)
-
-                port_1.line.connection = line_1
-                port_1.line.save()
-                port_2.line.delete()
-                port_2.line = port_1.line
-            port_1.save()
-            port_2.save()
+    def _connection_port(self, line: Line) -> None:
+        line_ports_id = [port['id'] for port in line.connection]
+        ports_update = []
+        for ids in line_ports_id:
+            ports_update.append([Port(id=ids, line=line)])
+        Port.objects.bulk_update(ports_update, ['line'])
 
     @property
     @lru_cache()
