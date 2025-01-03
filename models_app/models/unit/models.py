f98395582e26a59f4c0fc32686e2b20dc8519c3c
Долгополов Дмитрий <dmitry.dolgopolov@saltpepper.ru>
2025-01-03 21:06:47 +0300

    adsasd

--- models_app/models/unit/models.py
+++ models_app/models/unit/models.py
@@ -50,5 +50,5 @@ def check_free_unit(sender, instance, created, **kwargs):
         from models_app.models import Equipment
         server_rack.free_units = server_rack.number_of_units - Equipment.objects.filter(
             unit__in=server_rack.units.values("id")
-        ).distinct().aggregate(total=Sum('template__count_port'))["total"]
+        ).distinct().aggregate(total=Sum('template__number_of_units'))["total"]
         instance.server_rack.save()
