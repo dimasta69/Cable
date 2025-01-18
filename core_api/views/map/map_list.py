27930b6f34a94f8fcd00b4bf954f7e700eb2f255
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-01-18 18:56:38 +0300

    dasda

--- core_api/views/map/map_list.py
+++ core_api/views/map/map_list.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 from rest_framework.response import Response
 
+from core_api.services.map.create import CreateMapService
 from utils.services import ServiceOutcome
 from core_api.services.map.map_list import MapListService
 from core_api.serializers.map.resourse import MapSerializer
@@ -19,3 +20,9 @@ class MapListView(APIView):
         if bool(outcome.errors):
             return Response(outcome.errors, status=outcome.response_status)
         return Response(MapSerializer(outcome.result, many=True).data, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        outcome = ServiceOutcome(CreateMapService, {"current_user": request.user} | request.data)
+        if bool(outcome.errors):
+            return Response(outcome.errors, status=outcome.response_status)
+        return Response(MapSerializer(outcome.result).data, status=status.HTTP_201_CREATED)
