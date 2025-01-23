be689ea273f9a9693258a68505bd19212d03d33d
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-01-23 16:28:36 +0300

    asdasd

--- core_api/services/map/equipment_list.py
+++ core_api/services/map/equipment_list.py
@@ -47,7 +47,7 @@ class EquipmentListService(ServiceWithResult):
     def map_presence(self) -> None:
         if not self._map:
             self.add_error(
-                "map_id",
+                "id",
                 NotFound(
                     f"Map id={self.cleaned_data['id']} not found"
                 )
@@ -55,7 +55,7 @@ class EquipmentListService(ServiceWithResult):
             self.response_status = status.HTTP_404_NOT_FOUND
 
     def access_presence(self) -> None:
-        if self.cleaned_data['map_id']:
+        if self.cleaned_data['id']:
             if not self._access and not self.cleaned_data['current_user'].is_superuser:
                 self.add_error('current_user', PermissionDenied('Access to the map id = '
                                                                 f'{self.cleaned_data["id"]} is not granted'))
