6acdcf4d8ce452e28f7df6d8893b0bdcef5251df
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-01-26 11:31:45 +0300

    dasdad

--- core_api/utils/connection.py
+++ core_api/utils/connection.py
@@ -108,21 +108,20 @@ def disconnect(port_1, port_2) -> tuple[Line | None, Line | None]:
             line_1 = None
             line_2 = None
 
-            if line[i + 1]["port_id"] == str(port_2.id):
-                if line_is_null(line[:i + 1]):
-                    line_1 = Line.objects.create(connection=line[:i + 1])
-                if line_is_null(line[i + 1:]):
-                    line_2 = Line.objects.create(connection=line[i + 1:])
+            try:
+                if line[i + 1]["port_id"] == str(port_2.id):
+                    if line_is_null(line[:i + 1]):
+                        line_1 = Line.objects.create(connection=line[:i + 1])
+                    if line_is_null(line[i + 1:]):
+                        line_2 = Line.objects.create(connection=line[i + 1:])
+            except IndexError:
+                if line[i - 1]["port_id"] == str(port_2.id):
+                    if line_is_null(line[:i + 1]):
+                        line_2 = Line.objects.create(connection=line[:i + 1])
+                    if line_is_null(line[i - 1:]):
+                        line_1 = Line.objects.create(connection=line[i - 1:])
+            finally:
                 return line_1, line_2
-
-            elif line[i - 1]["port_id"] == str(port_2.id):
-                if line_is_null(line[:i + 1]):
-                    line_2 = Line.objects.create(connection=line[:i + 1])
-                if line_is_null(line[i - 1:]):
-                    line_1 = Line.objects.create(connection=line[i - 1:])
-                return line_1, line_2
-            else:
-                DisconnectionValueNotFound("Порты, требуемые для отключения, не найдены")
 
 
 def line_is_null(line: list) -> list | None:
