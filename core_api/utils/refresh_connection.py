ff1e696c29401d5d9aaa190aae09c44bf690927c
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-01-26 14:46:58 +0300

    adada

--- core_api/utils/refresh_connection.py
+++ core_api/utils/refresh_connection.py
@@ -4,26 +4,47 @@ from models_app.models import EquipmentScheme, Port
 
 
 def refresh_connection_is_active(equipment_scheme: EquipmentScheme) -> None:
-    ports = Port.objects.filter(equipment=equipment_scheme.equipment, line__isnull=False).select_related('line')
+    ports = Port.objects.filter(equipment=equipment_scheme.equipment, line__isnull=False).select_related(
+        'line',
+        'equipment',
+        'equipment__template__type',
+    )
 
     equipments_active = set()
     equipments_is_not_active = set()
 
-    for port in ports:
-        port_connection = port.line.connection
-        for i in range(len(port_connection)):
-            if port_connection[i]['equipment_is_active'] == "True":
-                equipments_active.add(int(port_connection[i]["equipment_id"]))
-            if int(port_connection[i]['equipment_id']) == equipment_scheme.equipment.id:
-                try:
-                    equipments_is_not_active.add(int(port_connection[i - 1]["equipment_id"]))
-                except IndexError:
-                    pass
+    if equipment_scheme.equipment.type.is_active:
+        for port in ports:
+            port_connection = port.line.connection
+            for i in range(len(port_connection)):
+                if port_connection[i]['equipment_is_active'] == "True":
+                    equipments_active.add(int(port_connection[i]["equipment_id"]))
+                if int(port_connection[i]['equipment_id']) == equipment_scheme.equipment.id:
+                    try:
+                        equipments_is_not_active.add(int(port_connection[i - 1]["equipment_id"]))
+                    except IndexError:
+                        pass
 
-                try:
-                    equipments_is_not_active.add(int(port_connection[i + 1]["equipment_id"]))
-                except IndexError:
-                    pass
+                    try:
+                        equipments_is_not_active.add(int(port_connection[i + 1]["equipment_id"]))
+                    except IndexError:
+                        pass
+
+    elif not equipment_scheme.equipment.type.is_active:
+        equipments_active = None
+        for port in ports:
+            port_connection = port.line.connection
+            for i in range(len(port_connection)):
+                if int(port_connection[i]['equipment_id']) == equipment_scheme.equipment.id:
+                    try:
+                        equipments_is_not_active.add(int(port_connection[i - 1]["equipment_id"]))
+                    except IndexError:
+                        pass
+
+                    try:
+                        equipments_is_not_active.add(int(port_connection[i + 1]["equipment_id"]))
+                    except IndexError:
+                        pass
 
     equipment_scheme.connection_active = list(equipments_active)
     equipment_scheme.connection_passive = list(equipments_is_not_active)
