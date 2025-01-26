f89aa0cd8334ad3017839289dd42dee4ca009bf4
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-01-26 15:03:10 +0300

    adada

--- core_api/utils/refresh_connection.py
+++ core_api/utils/refresh_connection.py
@@ -19,6 +19,7 @@ def refresh_connection_is_active(equipment_scheme: EquipmentScheme) -> None:
             for i in range(len(port_connection)):
                 if port_connection[i]['equipment_is_active'] == "True":
                     equipments_active.add(int(port_connection[i]["equipment_id"]))
+                breakpoint()
                 if int(port_connection[i]['equipment_id']) == equipment_scheme.equipment.id:
                     try:
                         equipments_is_not_active.add(int(port_connection[i - 1]["equipment_id"]))
