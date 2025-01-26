c6b40b0c67c00e7c2c5a743b4e36f8aceaf5eaf7
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-01-26 13:41:15 +0300

    adasda

--- core_api/services/equipment/equipments_from_map.py
+++ core_api/services/equipment/equipments_from_map.py
@@ -69,7 +69,12 @@ class EquipmentsFromMapListService(ServiceWithResult):
     @property
     def equipment_list(self):
         try:
-            return Equipment.objects.all().select_related('template', 'template__manufacturer', 'connections')
+            return Equipment.objects.all().select_related(
+                'template',
+                'template__manufacturer',
+            ).prefetch_related(
+                'connections',
+            )
         except Equipment.DoesNotExist:
             return Equipment.objects.none()
 
