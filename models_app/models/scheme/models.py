5b42af9f383f177a34d6527b8bdeb463ee0c1fb1
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-01-29 12:10:16 +0300

    sdasda

--- models_app/models/scheme/models.py
+++ models_app/models/scheme/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
 from models_app.models.base_model import BaseModel
+from models_app.models import Access
 
 
 class Scheme(BaseModel):
@@ -18,3 +21,7 @@ class Scheme(BaseModel):
 
     def __str__(self):
         return str(self.title)
+
+@receiver(post_delete, sender=Scheme)
+def delete_access(sender, instance, **kwargs):
+    Access.object.delete(object=instance)
