aa5c32c9a6de46ffc4df0574f42361baed34e235
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-01-29 15:23:11 +0300

    sdasd

--- core_api/services/access/list.py
+++ core_api/services/access/list.py
@@ -60,7 +60,7 @@ class AccessListService(ServiceWithResult):
             )
             access_list = access_filter | access_list.exclude(
                 role="Read", object_type=self.scheme_content_type, user__id__in=access_list.values("user__id")
-            )
+            ) if len(access_list) > 0 else access_list
         if self.cleaned_data["filter_room_id"]:
             access_filter = _access(
                 ContentType.objects.get_for_model(Room), self._room.pk,
