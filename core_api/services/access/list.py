959c7fa05871b1e33586d225be5f6b74e213a95b
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-01-29 14:38:38 +0300

    sdasd

--- core_api/services/access/list.py
+++ core_api/services/access/list.py
@@ -59,10 +59,9 @@ class AccessListService(ServiceWithResult):
                 ContentType.objects.get_for_model(Building), self._building.pk,
             )
             breakpoint()
-            access_list = access_filter | access_list
-            access_list.exclude(
+            access_list = access_filter.exclude(
                 role="Read", object_type=self.scheme_content_type, user__id__in=access_list.values("user__id")
-            )
+            ) | access_list
         if self.cleaned_data["filter_room_id"]:
             access_filter = _access(
                 ContentType.objects.get_for_model(Room), self._room.pk,
