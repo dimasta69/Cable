4676f7bea4acc2e05cc8eef873a66b781f24692a
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-01 21:38:55 +0300

    dadad

--- core_api/urls/port.py
+++ core_api/urls/port.py
@@ -1,6 +1,5 @@
 from django.urls import path
 
-from core_api.views.port.add_sfp import AddSfpView
 from core_api.views.port.disconnect_sfp import DisconnectSfpView
 from core_api.views.port.port import PortView
 from core_api.views.port.list import PortListView
@@ -8,6 +7,5 @@ from core_api.views.port.list import PortListView
 urlpatterns = [
     path('', PortListView.as_view()),
     path('<int:id>/', PortView.as_view()),
-    path('<int:id>/add_sfp/', AddSfpView.as_view()),
     path('disconnect_sfp/', DisconnectSfpView.as_view()),
 ]
