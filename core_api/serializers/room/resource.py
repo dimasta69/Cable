9521a004c9a8c979a541551c32c55370268c65c4
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-03 20:35:56 +0300

    dasda

--- core_api/serializers/room/resource.py
+++ core_api/serializers/room/resource.py
@@ -1,10 +1,8 @@
 from rest_framework import serializers
-from core_api.serializers.equipment.list import EquipmentListSerializer
 
 
 class RoomListSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=True)
     number = serializers.CharField(required=True)
     is_server_room = serializers.BooleanField(required=True)
-    equipments = EquipmentListSerializer(many=True, required=False)
     floor = serializers.IntegerField(required=False)
