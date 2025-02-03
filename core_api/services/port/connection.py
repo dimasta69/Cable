b0a531650c2ad19ef987a3ad064c5de96063690d
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-03 22:45:36 +0300

    dasda

--- core_api/services/port/connection.py
+++ core_api/services/port/connection.py
@@ -103,16 +103,15 @@ class ConnectionPortService(ServiceWithResult):
                     role__in=['Change', 'Creator'],
                 )
             if self._ports[port_id].equipment.units:
-                breakpoint()
                 return (
                         Access.objects.filter(
                             Q(
                                 object_type=self.building_content_type,
-                                object_id=self._ports[port_id].equipment.units[0].server_rack.room.building.id
+                                object_id=self._ports[port_id].equipment.units.all()[0].server_rack.room.building.id
                             ) |
                             Q(
                                 object_type=self.room_content_type,
-                                object_id=self._ports[port_id].equipment.units[0].server_rack.id
+                                object_id=self._ports[port_id].equipment.units.all()[0].server_rack.id
                             ),
                         ) | access_list
                 ).filter(
