c3232b73924ec5bdb88e4ac6bd5b15a3a500ad48
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-03 17:27:54 +0300

    dadasd

--- models_app/models/vlan/device/models.py
+++ models_app/models/vlan/device/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from models_app.models.base_model import BaseModel
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
+from utils.errors import ValidationError
 
 
 class VlanDevice(BaseModel):
@@ -20,3 +24,12 @@ class VlanDevice(BaseModel):
         db_table = "vlan_device"
         verbose_name = "Vlan Device"
         verbose_name_plural = "Vlan device's"
+
+
+@receiver(pre_save, sender=VlanDevice)
+def check_mode(sender, instance, **kwargs):
+    if not instance.pk and instance.device_type.__class__.__name__ == "port":
+        if instance.device.mode.is_only_one_vlan and len(
+                VlanDevice.object.filter(device_type=instance.device_type, device_id=instance.device_id)
+        ) >= 1:
+            raise ValidationError("На этот порт может быть установлен только один Vlan, из-за установки его mode")
