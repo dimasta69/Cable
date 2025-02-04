51765f5b5f52edd7a12236939a42704f68ca74e2
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-04 18:59:59 +0300

    dasd

--- core_api/serializers/sfp_template/resource.py
+++ core_api/serializers/sfp_template/resource.py
@@ -1,11 +1,11 @@
 from rest_framework import serializers
-
-
+from core_api.serializers.speed.resource import SpeedListSerializer
+from core_api.serializers.line_type.resource import LineTypeSerializer
 
 class SfpTemplateListSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=True)
     manufacturer = serializers.CharField(source="manufacturer.name")
     name = serializers.CharField()
     type_port = serializers.CharField(source="type_port.name")
-    speed = serializers.ListField()
-    line_type = serializers.CharField()
+    speed = SpeedListSerializer()
+    line_type = LineTypeSerializer()
