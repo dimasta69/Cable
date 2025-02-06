3bab10bde808c36b45c4e9e2a6f774e465fdb14e
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-06 19:38:41 +0300

    dasda

--- core_api/views/vlan/vlan.py
+++ core_api/views/vlan/vlan.py
@@ -5,11 +5,21 @@ from rest_framework.permissions import IsAuthenticated
 
 from utils.services import ServiceOutcome
 from core_api.services.vlan.delete import DeleteVlanService
-
+from core_api.services.vlan.update import UpdateVlanService
+from core_api.serializers.vlan.resource import VlanSerializer
 
 
 class VlanView(APIView):
     permission_classes = [IsAuthenticated]
+
+    def patch(self, request, **kwargs):
+        outcome: ServiceOutcome = ServiceOutcome(
+            UpdateVlanService,
+            request.data | {"current_user": request.user} | kwargs,
+        )
+        if bool(outcome.errors):
+            return Response(outcome.errors, status=outcome.response_status)
+        return Response(VlanSerializer(outcome.result).data, status=status.HTTP_200_OK)
 
     def delete(self, request, **kwargs) -> Response:
         outcome: ServiceOutcome = ServiceOutcome(
