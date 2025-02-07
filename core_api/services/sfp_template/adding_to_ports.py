be6fe9357a30516269231443a9a13a0d51e6b75d
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 20:26:36 +0300

    dadasd

--- core_api/services/sfp_template/adding_to_ports.py
+++ core_api/services/sfp_template/adding_to_ports.py
@@ -153,6 +153,7 @@ class AddToPortSfpService(ServiceWithResult):
             item["port_template__line_type"] for item in self._port_list.values("port_template__line_type")
         }
 
+        breakpoint()
         if not set(line_types) & set(self._sfp_template.line_type.all()):
             self.add_error(
                 "id",
