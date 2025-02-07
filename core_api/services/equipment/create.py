3932396a9e5979f376c79563c0d0285502f083fb
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 19:41:28 +0300

    asdad

--- core_api/services/equipment/create.py
+++ core_api/services/equipment/create.py
@@ -67,11 +67,11 @@ class CreateEquipmentService(ServiceWithResult):
                 )|
                 Q(
                     object_type=self.room_content_type,
-                    object_id=self._scheme.buildings.rooms.values("id"),
+                    object_id=self._scheme.buildings.values("room"),
                 )|
                 Q(
                     object_type=self.server_rack_content_type,
-                    object_id=self._scheme.buildings.rooms.server_racks.values("id"),
+                    object_id=self._scheme.buildings.rooms.server_racks.values("room__server_rack"),
                 ),
             ).filter(
                 user=self.cleaned_data['current_user'],
