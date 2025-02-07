06dffe9d4c61106d6aa5f6fefe30e21d278d3ab1
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 18:24:54 +0300

    dasda

--- core_api/services/sfp_template/adding_to_ports.py
+++ core_api/services/sfp_template/adding_to_ports.py
@@ -139,7 +139,8 @@ class AddToPortSfpService(ServiceWithResult):
         if self._port_list and self._sfp_template:
             sfp_speeds = self._sfp_template.speed
             for port in self._port_list:
-                if not set(port.port_template.speed.all()) & set(sfp_speeds):
+                speed_set = set(port.port_template.speed.all())
+                if not speed_set & set(sfp_speeds):
                     self.add_error(
                         "id",
                         ValidationError(
