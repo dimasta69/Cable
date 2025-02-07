37319fc5eaf9ad66694f01ddfc8dc64b0f592319
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 20:05:08 +0300

    dasdsa

--- core_api/services/equipment/create.py
+++ core_api/services/equipment/create.py
@@ -96,7 +96,6 @@ class CreateEquipmentService(ServiceWithResult):
 
     def access_presence(self) -> None:
         if self._scheme:
-            breakpoint()
             if not self._access and not self.cleaned_data['current_user'].is_superuser:
                 self.add_error('current_user', PermissionDenied('Access to the schema id = '
                                                                 f'{self._scheme.id} is not granted'))
