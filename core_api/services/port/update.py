7826380eb386a5e3f8e1515e10507d3a2fdd9b10
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 17:46:07 +0300

    dasdasdas

--- core_api/services/port/update.py
+++ core_api/services/port/update.py
@@ -109,11 +109,11 @@ class UpdatePortService(ServiceWithResult):
                         Access.objects.filter(
                             Q(
                                 object_type=self.building_content_type,
-                                object_id=self._port.equipment.units.all()[0].server_rack.building.id
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.building.id
                             ) |
                             Q(
                                 object_type=self.room_content_type,
-                                object_id=self._port.equipment.units.all()[0].server_rack.id
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.id
                             ),
                         ) | access_list
                 ).filter(
