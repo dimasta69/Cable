5affb030f1af289ae916cb411952e0d5230c0d11
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-07 17:39:14 +0300

    dasdasdas

--- core_api/utils/change_mode_port.py
+++ core_api/utils/change_mode_port.py
@@ -6,4 +6,4 @@ port_content_type = ContentType.objects.get_for_model(Port)
 
 
 def change_mode_port(port_id: int) -> None:
-    VlanDevice.objects.filter(object_type=port_content_type, object_id=port_id).delete()
+    VlanDevice.objects.filter(device_type=port_content_type, device_id=port_id).delete()
