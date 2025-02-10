a2451e729113e40f481f44a761997e427ef6e85b
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-10 17:41:20 +0300

    cscsnb

--- core_api/services/building/delete.py
+++ core_api/services/building/delete.py
@@ -1,20 +1,22 @@
 from functools import lru_cache
 
 from django import forms
+from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ObjectDoesNotExist, PermissionDenied
 from rest_framework import status
 
-from models_app.models import Access, User
+from models_app.models import Access, User, Building, Scheme
 from utils.fields import ModelField
 from utils.services import ServiceWithResult
-from models_app.models import Building
 
 
 class DeleteBuildingService(ServiceWithResult):
     id = forms.IntegerField(required=True)
     current_user = ModelField(User)
 
-    custom_validations = ['building_presence', 'access_presence']
+    scheme_content_type = ContentType.objects.get_for_model(Scheme)
+
+    custom_validations = ["building_presence", "access_presence"]
 
     def process(self):
         self.run_custom_validations()
@@ -31,7 +33,7 @@ class DeleteBuildingService(ServiceWithResult):
     @lru_cache()
     def _building(self) -> Building | None:
         try:
-            return Building.objects.get(id=self.cleaned_data['id'])
+            return Building.objects.get(id=self.cleaned_data["id"])
         except Building.DoesNotExist:
             return None
 
@@ -39,9 +41,9 @@ class DeleteBuildingService(ServiceWithResult):
     def _access(self) -> Access | None:
         try:
             return Access.objects.get(
-                user=self.cleaned_data['current_user'],
+                user=self.cleaned_data["current_user"],
                 object_id=self._building.scheme,
-                role__in=['Change', 'Creator'],
+                role__in=["Change", "Creator"],
                 object_type=self.scheme_content_type,
             )
         except Access.DoesNotExist:
@@ -49,13 +51,22 @@ class DeleteBuildingService(ServiceWithResult):
 
     def building_presence(self) -> None:
         if not self._building:
-            self.add_error('id', ObjectDoesNotExist('Building id='
-                                                    f'{self.cleaned_data["id"]} not found'))
+            self.add_error(
+                "id",
+                ObjectDoesNotExist(
+                    "Building id=" f'{self.cleaned_data["id"]} not found'
+                ),
+            )
             self.response_status = status.HTTP_404_NOT_FOUND
 
     def access_presence(self) -> None:
         if self._building:
-            if not self._access and not self.cleaned_data['current_user'].is_superuser:
-                self.add_error('current_user', PermissionDenied('Access to the schema id = '
-                                                                f'{self._building.scheme.id} is not granted'))
+            if not self._access and not self.cleaned_data["current_user"].is_superuser:
+                self.add_error(
+                    "current_user",
+                    PermissionDenied(
+                        "Access to the schema id = "
+                        f"{self._building.scheme.id} is not granted"
+                    ),
+                )
                 self.response_status = status.HTTP_403_FORBIDDEN
