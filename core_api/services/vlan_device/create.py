7b10f8b5cc40bf2994e181a1f9b707c131a368e2
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-15 18:46:11 +0300

    fsfsfa

--- core_api/services/vlan_device/create.py
+++ core_api/services/vlan_device/create.py
@@ -123,11 +123,15 @@ class CreateVlanDeviceService(ServiceWithResult):
                         Access.objects.filter(
                             Q(
                                 object_type=self.building_content_type,
-                                object_id=self._port.equipment.units[0].server_rack.building.id
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.building.id
                             ) |
                             Q(
                                 object_type=self.room_content_type,
-                                object_id=self._port.equipment.units[0].server_rack.id
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.id
+                            ) |
+                            Q(
+                                object_type=self.server_rack_content_type,
+                                object_id=self._port.equipment.units.all()[0].server_rack.id
                             ),
                         ) | access_list
                 ).filter(
