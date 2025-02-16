631f2fd0e37274806bf9aeb4eb08a1acaffe1912
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-17 00:33:55 +0300

    sdfaasdfs

--- core_api/utils/refresh_connection_building.py
+++ core_api/utils/refresh_connection_building.py
@@ -1,78 +1,34 @@
-from models_app.models import Equipment, ServerRack, Building, Unit
-from typing import List
-from models_app.models.scheme.models import Scheme
+from models_app.models import Building, Scheme
 from models_app.models.port.models import Port
 from django.db import transaction
-
-
-# def refresh_connection_building(building: Building) -> None:
-#     building_connection = []
-#     building_connection.append(
-#         [
-#             list(
-#                 map(refresh_connection_server_rack, server_rack)
-#             ) for server_rack in building.rooms.all().values_list("server_rack")
-#         ]
-#     )
-#     building.connection = [set(building_connection)]
-#     building.save()
-
-
-# def refresh_connection_server_rack(server_rack_id: int) -> list[int]:
-#     equipment_connection = equipment_connection_passive(server_rack_id)
-#     server_rack_connection = port_connection_back_side(equipment_connection)
-#     return [set(server_rack_connection)]
-
-
-# def equipment_connection_passive(server_rack_id: int) -> List[Equipment]:
-#     equipments_passive_list = [
-#         unit.equipment
-#         for unit in server_rack.units.all()
-#         if unit.equipment and unit.equipment.template.type.is_active is False
-#     ]
-
-#     equipments_passive_list = Unit.objects.filter(
-#         server_rack_id=server_rack_id, equipment__isnull=False, equipment__template__type__is_active=False
-#     )
-#     return [equipments_passive_list]
-
-
-# def port_connection_back_side(equipment_passive_list: List[Equipment]) -> list[int]:
-#     server_rack_connection = []
-#     for equipment in equipment_passive_list:
-#         server_rack_connection.append(
-#             [
-#                 port.equipment.units.all()[0].server_rack.room.building.id
-#                 for port in equipment.ports if port.back_side is not None
-#             ]
-#         )
-#     return server_rack_connection
-
-
-# def refresh_connection_building(building: Building, scheme) -> None:
-#     building_connection = []
-#     building_connection.append(
-#         [
-#             list(
-#                 map(refresh_connection_server_rack, ServerRack.objects.filter(room__building__scheme=scheme, ))
-#             ) for server_rack in building.rooms.all().values_list("server_rack")
-#         ]
-#     )
-#     building.connection = [set(building_connection)]
-#     building.save()
+from typing import List, Dict
 
 
 def refresh_connection_building(scheme: Scheme) -> None:
-    building_connection = {}
+    building_to_update = prepared_building_to_update()
+    with transaction.atomic():
+        Building.objects.bulk_update(building_to_update, ['connection'])
+
+
+def prepared_building_to_update(scheme: Scheme) -> List[Building]:
+    ports = port_list(scheme)
+    building_connection = set_building_connection(ports)
+    building_to_update = create_buildings_to_update(building_connection)
+    return building_to_update
+
+
+def port_list(scheme: Scheme) -> List[Port]:
     ports = Port.objects.filter(
         equipment__scheme=scheme,
         equipment__template__type__is_active=False,
         back_side__isnull=False,
         back_side__equipment__room__isnull=True,
     )
+    return ports
 
+
+def set_building_connection(ports: List[Port]) -> Dict[Building, List[int]]:
     building_connection = {}
-
     for port in ports:
         building = port.equipment.units.first().server_rack.room.building
         connection_id = port.back_side.equipment.units.first().server_rack.room.building.id
@@ -81,11 +37,13 @@ def refresh_connection_building(scheme: Scheme) -> None:
             building_connection[building].append(connection_id)
         else:
             building_connection[building] = [connection_id]
+    return building_connection
 
-    objects_to_update = []
+
+def create_buildings_to_update(building_connection: Dict[Building, List[int]]) -> List[Building]:
+    building_to_update = []
     for building, connection in building_connection.items():
         building.connection = connection
-        objects_to_update.append(building)
+        building_to_update.append(building)
 
-    with transaction.atomic():
-        Building.objects.bulk_update(objects_to_update, ['connection'])
+    return building_to_update
