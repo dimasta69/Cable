82cbd8f7d97b3f5fa0913d7877b4f1d7a22e07a6
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-19 14:36:34 +0300

    dadssa

--- models_app/models/scheme/models.py
+++ models_app/models/scheme/models.py
@@ -24,6 +24,7 @@ class Scheme(BaseModel):
 @receiver(post_delete, sender=Scheme)
 def delete_access(sender, instance, **kwargs):
     from models_app.models import Access
+    
     from django.contrib.contenttypes.models import ContentType
     content_type = ContentType.objects.get_for_model(Scheme)
     Access.objects.filter(object_type=content_type, object_id=instance.id).delete()
