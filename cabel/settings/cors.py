825c4972f3cc9bcab791fb1aa9075a0d19e8259d
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-19 20:50:19 +0300

    dasdasd

--- cabel/settings/cors.py
+++ cabel/settings/cors.py
@@ -3,9 +3,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost",
     "http://127.0.0.1:8000",
     "http://94.131.107.85",
-    "http://front:3000",
+    "http://front:5173",
 ]
-CSRF_TRUSTED_ORIGINS = ["http://212.67.12.63","http://localhost","http://127.0.0.1:8000", "http://front:3000",
+CSRF_TRUSTED_ORIGINS = ["http://212.67.12.63","http://localhost","http://127.0.0.1:8000", "http://front:5173",
                         "http://94.131.107.85"]
 
 CORS_ALLOW_HEADERS = [
