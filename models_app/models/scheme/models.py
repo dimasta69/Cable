0656670d704a3d0f8da20b23853aa07373485b89
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-19 14:07:33 +0300

    vdfeg

--- models_app/models/scheme/models.py
+++ models_app/models/scheme/models.py
@@ -1,4 +1,3 @@
-from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.db.models.signals import post_delete
 from django.dispatch import receiver
@@ -25,5 +24,6 @@ class Scheme(BaseModel):
 @receiver(post_delete, sender=Scheme)
 def delete_access(sender, instance, **kwargs):
     from models_app.models import Access
+    from django.contrib.contenttypes.models import ContentType
     content_type = ContentType.objects.get_for_model(Scheme)
     Access.objects.filter(object_type=content_type, object_id=instance.id).delete()
