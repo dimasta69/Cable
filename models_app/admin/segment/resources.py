9db92e32b4c29d92900b9d04ab4e069801cc07a8
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-21 16:08:04 +0300

    Change segment admin panel

--- models_app/admin/segment/resources.py
+++ models_app/admin/segment/resources.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from models_app.models import Segment
+from models_app.models import Segment, Vlan
+
+
+class VlanInline(admin.TabularInline):
+    model = Vlan
+    fields = ('id', 'name',)
+    extra = 0
 
 
 class SegmentAdmin(admin.ModelAdmin):
@@ -8,6 +14,9 @@ class SegmentAdmin(admin.ModelAdmin):
     list_display = ('id', "name", "scheme", 'created_at')
     search_fields = ('id', 'name', 'scheme')
     ordering = ('-created_at',)
+    inlines = (
+        VlanInline,
+    )
 
 
 admin.site.register(Segment, SegmentAdmin)
