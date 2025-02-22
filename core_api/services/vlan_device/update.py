130078cb005f58d866ae5b579bf8fea97843f0ac
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-02-22 18:48:48 +0300

    dfsdfe

--- core_api/services/vlan_device/update.py
+++ core_api/services/vlan_device/update.py
@@ -63,6 +63,7 @@ class UpdateVlanDeviceService(ServiceWithResult):
         scheme_content_type = ContentType.objects.get_for_model(Scheme)
         building_content_type = ContentType.objects.get_for_model(Building)
         room_content_type = ContentType.objects.get_for_model(Room)
+        server_rack_content_type = ContentType.objects.get_for_model(ServerRack)
         equipment_content_type = ContentType.objects.get_for_model(Equipment)
         try:
             access_list = Access.objects.filter(
@@ -96,12 +97,16 @@ class UpdateVlanDeviceService(ServiceWithResult):
                         Access.objects.filter(
                             Q(
                                 object_type=building_content_type,
-                                object_id=self._port.equipment.units[0].server_rack.building.id
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.building.id
                             ) |
                             Q(
                                 object_type=room_content_type,
-                                object_id=self._port.equipment.units[0].server_rack.id
-                            ),
+                                object_id=self._port.equipment.units.all()[0].server_rack.room.id
+                            ) |
+                            Q(
+                                object_type=server_rack_content_type,
+                                object_id=self._port.equipment.units.all()[0].server_rack.id
+                            )
                         ) | access_list
                 ).filter(
                     user=self.cleaned_data['current_user'],
@@ -115,6 +120,7 @@ class UpdateVlanDeviceService(ServiceWithResult):
         scheme_content_type = ContentType.objects.get_for_model(Scheme)
         building_content_type = ContentType.objects.get_for_model(Building)
         room_content_type = ContentType.objects.get_for_model(Room)
+        server_rack_content_type = ContentType.objects.get_for_model(ServerRack)
         equipment_content_type = ContentType.objects.get_for_model(Equipment)
         try:
             access_list = Access.objects.filter(
@@ -148,12 +154,16 @@ class UpdateVlanDeviceService(ServiceWithResult):
                         Access.objects.filter(
                             Q(
                                 object_type=building_content_type,
-                                object_id=self._equipment.units[0].server_rack.building.id
+                                object_id=self._equipment.units.all()[0].server_rack.building.id
                             ) |
                             Q(
                                 object_type=room_content_type,
-                                object_id=self._equipment.units[0].server_rack.id
-                            ),
+                                object_id=self._equipment.units.all()[0].server_rack.room.id
+                            ) |
+                            Q(
+                                object_type=server_rack_content_type,
+                                object_id=self._equipment.units.all()[0].server_rack.id
+                            )
                         ) | access_list
                 ).filter(
                     user=self.cleaned_data['current_user'],
