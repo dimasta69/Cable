9ad622d1fe9851cb8a9dc600ca793103a06d181e
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-02-24 00:25:24 +0300

    dasdasd

--- core_api/services/equipment/delete.py
+++ core_api/services/equipment/delete.py
@@ -40,6 +40,7 @@ class DeleteEquipmentService(ServiceWithResult):
         except Equipment.DoesNotExist:
             return None
 
+    @property
     def _access(self, port_id: int) -> List[Access] | None:
         scheme_content_type = ContentType.objects.get_for_model(Scheme)
         building_content_type = ContentType.objects.get_for_model(Building)
@@ -99,7 +100,7 @@ class DeleteEquipmentService(ServiceWithResult):
 
     def access_presence(self) -> None:
         if self._equipment:
-            if (not self._access or not self._access.exists()) and not self.cleaned_data['current_user'].is_superuser:
+            if not self._access and not self.cleaned_data['current_user'].is_superuser:
                 self.add_error('current_user', PermissionDenied('Access to the schema id = '
                                                                 f'{self._equipment.scheme.id} is not granted'))
                 self.response_status = status.HTTP_403_FORBIDDENas
