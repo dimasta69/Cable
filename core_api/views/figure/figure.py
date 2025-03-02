325e3e3acf50356113257854b7530013f543ef57
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-03-02 18:48:47 +0300

    dasdasd

--- core_api/views/figure/figure.py
+++ core_api/views/figure/figure.py
@@ -1,4 +1,3 @@
-import django.urls
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
@@ -7,6 +6,7 @@ from rest_framework.response import Response
 from utils.services import ServiceOutcome
 from core_api.serializers.figure.resource import FigureSerializer
 from core_api.services.figure.update import UpdateFigureService
+from core_api.services.figure.delete import DeleteFigureService
 
 
 class UpdateFigureView(APIView):
@@ -19,4 +19,13 @@ class UpdateFigureView(APIView):
         )
         if bool(outcome.errors):
             return Response(outcome.errors, status=outcome.response_status)
-        return Response(FigureSerializer(outcome.result).data, status=status.HTTP_201_CREATED)
+        return Response(FigureSerializer(outcome.result).data, status=status.HTTP_200_OK)
+
+    def delete(self, request, **kwargs) -> Response:
+        outcome: ServiceOutcome = ServiceOutcome(
+            DeleteFigureService,
+            {"current_user": request.user} | kwargs,
+        )
+        if bool(outcome.errors):
+            return Response(outcome.errors, status=outcome.response_status)
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
