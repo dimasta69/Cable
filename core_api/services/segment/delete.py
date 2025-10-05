5a789dc8fb1b82867fbdad30665bff4dd8174759
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-10-05 17:41:55 +0300

    Add delete_segment service

---
+++ core_api/services/segment/delete.py
@@ -0,0 +1,63 @@
+from django import forms
+from functools import lru_cache
+
+from django.contrib.contenttypes.models import ContentType
+from rest_framework import status
+from rest_framework.exceptions import PermissionDenied, NotFound
+
+from utils.fields import ModelField
+from utils.services import ServiceWithResult
+from models_app.models import Access, User, Segment, Scheme
+
+class DeleteSegmentService(ServiceWithResult):
+    id = forms.IntegerField(required=True)
+    current_user = ModelField(User)
+
+    custom_validations = ["segment_presence", "access_presence"]
+
+    def process(self):
+        self.run_custom_validations()
+        if self.is_valid():
+            self.result = self._segment.delete()
+            self.response_status = status.HTTP_204_NO_CONTENT
+        return self
+
+    @property
+    @lru_cache()
+    def _segment(self) -> Segment | None:
+        try:
+            return Segment.objects.select_related("scheme").get(id=self.cleaned_data['id'])
+        except Segment.DoesNotExist:
+            return None
+
+    @property
+    def _access(self):
+        scheme_content_type = ContentType.objects.get_for_model(Scheme)
+        try:
+            return Access.objects.filter(
+                Q(
+                    object_type=scheme_content_type,
+                    object_id=self._segment.scheme.pk,
+                ),
+            ).filter(
+                user=self.cleaned_data['current_user'],
+                role__in=['Change', 'Creator']
+            )
+        except Access.DoesNotExist:
+            return None
+
+    def segment_presence(self) -> None:
+        self.add_error(
+            "id",
+            NotFound(
+                f"Segment id={self.cleaned_data['id']} not found"
+            )
+        )
+        self.response_status = status.HTTP_404_NOT_FOUND
+
+    def access_presence(self) -> None:
+        if self._segment:
+            if not self._access and not self.cleaned_data['current_user'].is_superuser:
+                self.add_error('current_user', PermissionDenied('Access to the schem id = '
+                                                                f'{self._segment.scheme.id} is not granted'))
+                self.response_status = status.HTTP_403_FORBIDDEN
--- core_api/views/segment/segment.py
+++ core_api/views/segment/segment.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from utils.services import ServiceOutcome
 from core_api.serializers.segment.resource import SegmentSerializer
 from core_api.services.segment.update import UpdateSegmentService
+from core_api.services.segment.delete import DeleteSegmentService
 
 class SegmentView(APIView):
     permission_classes = [IsAuthenticated]
@@ -21,4 +22,17 @@ class SegmentView(APIView):
             )
         return Response(
             SegmentSerializer(outcome.result).data, status=status.HTTP_200_OK
+        )
+
+    def delete(self, request) -> Response:
+        outcome: ServiceOutcome = ServiceOutcome(
+            DeleteSegmentService,
+            request.data | {"current_user": request.user},
+        )
+        if bool(outcome.errors):
+            return Response(
+                outcome.errors, status=outcome.response_status
+            )
+        return Response(
+            SegmentSerializer(outcome.result).data, status=outcome.response_status
         )
