eaaccb525b245790a99733e8f133052116a2b15a
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-10-11 15:11:57 +0300

    Change serializer from user_list

--- core_api/serializers/users/resource.py
+++ core_api/serializers/users/resource.py
@@ -1,6 +1,18 @@
 from rest_framework import serializers
 
 
+class AccessSerializers(serializers.Serializer):
+    role = serializers.CharField()
+    object_id = serializers.IntegerField()
+    object_type = serializers.CharField(source='object_type.model')
+
+
 class UsersListSerializers(serializers.Serializer):
     id = serializers.IntegerField()
     username = serializers.CharField()
+    access = serializers.SerializerMethodField()
+
+    def get_access(self, obj):
+        if obj.access.exists():
+            return AccessSerializers(obj.access.all(), many=True).data
+        return []
