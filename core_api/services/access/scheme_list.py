41aab959b973b612a33b4cfc3628af32db58f7ab
Dmitry Dolgopolov <DMSTVKO@gmail.com>
2025-10-14 18:01:45 +0300

    Change AccessListService add filter_user_id

--- core_api/services/access/scheme_list.py
+++ core_api/services/access/scheme_list.py
@@ -16,10 +16,11 @@ class AccessListService(ServiceWithResult):
     page = forms.IntegerField(required=False)
     per_page = forms.IntegerField(required=False)
     filter_scheme_id = forms.IntegerField(required=True)
+    filter_user_id = forms.IntegerField(required=False)
     filter_role = forms.CharField(required=False)
     search_filter = forms.CharField(required=False)
 
-    custom_validations = ['scheme_presence', 'access_owner_or_superuser', 'filter_role_presence']
+    custom_validations = ['scheme_presence', 'access_owner_or_superuser', 'filter_role_presence', 'user_presence']
 
     def process(self):
         self.run_custom_validations()
@@ -34,6 +35,8 @@ class AccessListService(ServiceWithResult):
             else self._access.exclude(user=self.cleaned_data['current_user'])
         if self.cleaned_data['filter_role']:
             access_list = access_list.filter(role=self.cleaned_data['filter_role'])
+        if self.cleaned_data["filter_user_id"]:
+            access_list = access_list.filter(user=self.user)
         if self.cleaned_data['search_filter']:
             access_list = access_list.filter(
                 Q(user__username__icontains=self.cleaned_data['search_filter']) |
@@ -54,11 +57,27 @@ class AccessListService(ServiceWithResult):
 
     @property
     @lru_cache()
+    def user(self) -> User | None:
+        try:
+            return User.objects.filter(id=self.cleaned_data['filter_user_id'])
+        except User.DoesNotExist:
+            return None
+
+    @property
+    @lru_cache()
     def _scheme(self) -> Scheme | None:
         try:
             return Scheme.objects.get(id=self.cleaned_data['filter_scheme_id'])
         except Scheme.DoesNotExist:
             return None
+
+    def user_presence(self) -> None:
+        if self.cleaned_data['filter_user_id']:
+            if not self.user:
+                self.add_error('filter_user_id', ObjectDoesNotExist('User id='
+                                                                      f'{self.cleaned_data["filter_user_id"]} '
+                                                                      'not found'))
+                self.response_status = status.HTTP_404_NOT_FOUND
 
     def filter_role_presence(self) -> None:
         if self.cleaned_data['filter_role']:
