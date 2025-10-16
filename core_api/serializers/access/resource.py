fdf1a4ac9768cdc0cce52455f92c499dfe7582a0
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-10-16 16:02:40 +0300

    Add objecct_type to AccessListSerializer

--- core_api/serializers/access/resource.py
+++ core_api/serializers/access/resource.py
@@ -5,8 +5,9 @@ from models_app.models import Access
 
 class AccessListSerializer(serializers.Serializer):
     id = serializers.IntegerField(required=True)
-    user = serializers.SerializerMethodField()
+    user = serializers.CharField(resource="object_type.model")
     role = serializers.CharField(required=True)
+    object_type = serializers.SerializerMethodField()
 
     def get_user(self, obj: Access) -> dict[str, str]:
         return {
