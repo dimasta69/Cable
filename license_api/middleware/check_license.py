ec9883bc09b1af815e9a13d821710edeb8bf1976
Dmitry Dolgopolov <dmitry.dolgopolov@saltpepper.ru>
2025-12-08 20:17:37 +0300

    Change midlware

--- license_api/middleware/check_license.py
+++ license_api/middleware/check_license.py
@@ -41,8 +41,12 @@ class LicenseMiddleware:
 
         if parameters['mac_id'] != generate_stable_id_from_mac():
             raise LicenseMacException('Не совпадает mac устройства.')
-        if parameters['time_unlimited'] is None or parameters.get('period_end_date') < datetime.datetime.now():
-            raise LicenseTimeException(parameters.get('period_end_date'), datetime.datetime.now())
+        try:
+            if parameters['time_unlimited'] is None:
+                if parameters.get('period_end_date') < datetime.datetime.now():
+                    raise LicenseTimeException(parameters.get('period_end_date'), datetime.datetime.now())
+        except TypeError as e:
+            pass
 
         for model_name, count in parameters['restrictions'].items():
             if getattr(models, model_name).objects.all().count() >= int(count):
